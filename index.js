04c4863a0a5b5ff37641289f80fcc831122f2942
fizaayesha <fizaayesha696@gmail.com>
2022-11-15 17:34:59 +0530

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const UserRouter = require("./routes/user");
 const AuthRouter = require("./routes/auth");
-
+const ProductRouter = require("./routes/product");
+const OrderRouter = require("./routes/order");
+const CartRouter = require("./routes/cart");
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL)
@@ -17,6 +19,9 @@ mongoose
 app.use(express.json());
 app.use("/api/users", UserRouter);
 app.use("/api/auth", AuthRouter);
+app.use("/api/products", ProductRouter);
+app.use("/api/carts", CartRouter);
+app.use("/api/orders", ProductRouter);
 app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });
